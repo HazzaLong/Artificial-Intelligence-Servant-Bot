e6a10100a3b3020d9e784b175be349ae227d1498
HazzaLong <100433506+HazzaLong@users.noreply.github.com>
2022-06-30 15:43:05 -0700

    initial commit

---
+++ translateApp.py
@@ -0,0 +1,9 @@
+from tkinter import Tk
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Codemy.com - Translator')
+    root.iconbitmap('c:/codemy.ico')
+    root.geometry("880x300")
+
+    root.mainloop()
